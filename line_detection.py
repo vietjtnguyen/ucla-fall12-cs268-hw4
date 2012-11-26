e045123e31434667140c08a9320e19dfe45c3acb
Viet Nguyen <viet@vietjtnguyen.com>
2012-11-26 02:19:47 -0800

    Added a skeleton file for the line detection part of the homework.

---
+++ line_detection.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+'''
+Copyright (c) 2012 Viet Nguyen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+'''
+
+import math
+import sys
+
+import cv2
+import numpy as np
+
+import intrinsic_calibration
+
+if __name__ == '__main__':
+
+	image_paths = ['LDWS_test/LDWS_test_data {0:03}.bmp'.format(x) for x in range(1, 609)]
+
+	intrinsic_matrix, distortion_coefficients = intrinsic_calibration.hw4_calibration(False)
+
+	# end in an interactive shell so we can look at the values
+	try:
+		import IPython
+		IPython.embed()
+	except Exception as e:
+		sys.exit()
+
